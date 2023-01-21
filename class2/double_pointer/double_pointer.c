93e526d1c31e5db3ac01fd1c333c154d4b58ec51
Raihan-28011 <raihanrifar4@gmail.com>
2023-01-22 01:39:07 +0600

    flawed swap function for pointers

--- class2/double_pointer/double_pointer.c
+++ class2/double_pointer/double_pointer.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+    int *temp = a;
+    a = b;
+    b = temp;
 }
 
 int main() {
-    int a = 10, b = 20;
-    printf("Before swapping: a = %d, b = %d\n", a, b);
+    int a, b;
+    int *aptr = &a, *bptr = &b;
+    printf("Before swapping: aptr = %p, bptr = %p\n", aptr, bptr);
 
-    swap(&a, &b);
-    printf("After swapping: a = %d, b = %d\n", a, b);
+    swap(aptr, bptr);
+    printf("After swapping: aptr = %p, bptr = %p\n", aptr, bptr);
 }
