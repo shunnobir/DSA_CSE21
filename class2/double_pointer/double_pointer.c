94d44e509df446292866c068d3729115d41e2b9d
Raihan-28011 <raihanrifar4@gmail.com>
2023-01-22 01:39:38 +0600

    edited swap function for pointers

--- class2/double_pointer/double_pointer.c
+++ class2/double_pointer/double_pointer.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-    int *temp = a;
-    a = b;
-    b = temp;
+void swap(int **a, int **b) {
+    int *temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int main() {
@@ -11,6 +11,6 @@ int main() {
     int *aptr = &a, *bptr = &b;
     printf("Before swapping: aptr = %p, bptr = %p\n", aptr, bptr);
 
-    swap(aptr, bptr);
+    swap(&aptr, &bptr);
     printf("After swapping: aptr = %p, bptr = %p\n", aptr, bptr);
 }
